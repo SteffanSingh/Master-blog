6a8d7a184d9c054f665bcd09ea1718acbc7dbe54
unknown <samratkiev@gmail.com>
2023-07-27 19:12:31 +0200

    CRUD operation successful

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, url_for
-
+import uuid
 
 app = Flask(__name__)
 
@@ -14,14 +14,14 @@ with open("data.json", "r") as fileObject:
 @app.route('/')
 def index():
     # add code here to fetch the job posts from a file
-    return render_template('index.html', posts=blog_posts)
+    return render_template('index.html', posts=enumerate(blog_posts))
 
 @app.route("/add", methods= ["GET", "POST"])
 def add():
     if request.method == "POST":
         new_post = {}
+        id = (uuid.uuid1()).int
 
-        id = len(blog_posts)+1
         title = request.form["title"]
 
         content = request.form["content"]
@@ -33,7 +33,7 @@ def add():
         new_post["author"] = author
 
         blog_posts.append(new_post)
-        print(blog_posts)
+
         with open("data.json", "w") as fileObject:
             fileObject.write(json.dumps(blog_posts, indent=4))
 
@@ -42,7 +42,7 @@ def add():
 
 @app.route("/delete/<int:post_id>")
 def delete(post_id):
-    print(blog_posts)
+
     for index,blog in enumerate(blog_posts):
         print(type(blog))
         if blog["id"] == post_id:
@@ -56,26 +56,26 @@ def delete(post_id):
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
     # Fetch the blog posts from the JSON file
-    post = [blog for blog in blog_posts if blog["id"] == post_id ]
-    print(post)
+    post = [blog for blog in blog_posts if blog["id"] == post_id ][0]
     if post is None:
         # Post not found
         return "Post not found", 404
     if request.method == 'POST':
     # Update the post in the JSON file
+        id = post_id
         author =request.form["author"]
         title = request.form["title"]
         content = request.form["content"]
-        dict(post)['author'] = author
-        dict(post)["content"] = content
-        dict(post)["title"] = title
-        blog_posts[post_id - 1] = post
+        post['id'] = post_id
+        post['author'] = author
+        post["content"] = content
+        post["title"] = title
+
+
         with open("data.json", "w") as fileObject:
             fileObject.write(json.dumps(blog_posts, indent=4))
         # Redirect back to index
         return redirect(url_for("index"))
-    # Else, it's a GET request
-    # So display the update.html page
     return render_template('update.html', post=post)
 
 
